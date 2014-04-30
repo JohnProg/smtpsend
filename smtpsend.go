53f3e49d3d8c9215b6d7856772567c1d3a19227c
Robin Harper <cabrel@zerklabs.com>
2014-04-30 18:52:17 -0400

    Removed functionality that was replaced by github.com/zerklabs/libsmtp

--- smtpsend.go
+++ smtpsend.go
@@ -1,16 +1,10 @@
 package main
 
 import (
-	"bytes"
-	"crypto/tls"
-	"encoding/base64"
 	"flag"
 	"fmt"
-	"github.com/zerklabs/auburn"
-	"io/ioutil"
+	"github.com/zerklabs/libsmtp"
 	"log"
-	"net/smtp"
-	"path/filepath"
 	"strings"
 	"time"
 )
@@ -29,128 +23,24 @@ var (
 func main() {
 	flag.Parse()
 
-	if len(*server) == 0 {
-		log.Fatal("SMTP server required")
+	client, err := libsmtp.New(*server, *port, *from, strings.Split(*to, ","), *useTls)
+
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	if len(*from) == 0 {
-		log.Fatal("Mail sender required")
-	}
-
-	if len(*to) == 0 {
-		log.Fatal("Mail recipient(s) required")
-	}
-
-	buildMailMessage()
-}
-
-func buildMailMessage() {
-	boundary := auburn.RandomBase36()
-	buf := bytes.NewBuffer(nil)
-
-	buf.WriteString(fmt.Sprintf("From: %s\n", *from))
-	buf.WriteString(fmt.Sprintf("To: %s\n", *to))
-	buf.WriteString(fmt.Sprintf("Subject: %s\n", *subject))
-	buf.WriteString("MIME-version: 1.0;\n")
+	client.Subject(*subject)
+	client.Body(*body)
 
 	if len(*attachment) > 0 {
-		buf.WriteString(fmt.Sprintf("Content-Type: multipart/mixed; boundary=\"%s\"\n", boundary))
-		buf.WriteString(fmt.Sprintf("--%s\n", boundary))
-	}
-
-	buf.WriteString("Content-Type: text/plain; charset=\"UTF-8\";\n\n")
-	buf.WriteString(*body)
-
-	if len(*attachment) > 0 {
-		attachmentName := filepath.Base(*attachment)
-		b, err := ioutil.ReadFile(*attachment)
-
-		if err != nil {
-			log.Fatalf("Problem reading the given attachment:\n\t%s", err)
-		}
-
-		encodedLen := base64.StdEncoding.EncodedLen(len(b))
-		encodedAttachment := make([]byte, encodedLen)
-		base64.StdEncoding.Encode(encodedAttachment, b)
-
-		buf.WriteString(fmt.Sprintf("\n\n--%s\n", boundary))
-		buf.WriteString(fmt.Sprintf("Content-Type: application/octet-stream; name=\"%s\"\n", attachmentName))
-		buf.WriteString(fmt.Sprintf("Content-Description: %s\n", attachmentName))
-		buf.WriteString(fmt.Sprintf("Content-Disposition: attachment; filename=\"%s\"; size=%d\n", attachmentName, encodedLen))
-		buf.WriteString("Content-Transfer-Encoding: base64\n\n")
-
-		buf.Write(encodedAttachment)
-		buf.WriteString(fmt.Sprintf("\n--%s--", boundary))
-	}
-
-	smtpUri := fmt.Sprintf("%s:%d", *server, *port)
-
-	c, err := smtp.Dial(smtpUri)
-
-	if err != nil {
-		log.Fatalf("Error creating SMTP connection: %s", err)
-	}
-
-	if *useTls {
-		// check if TLS is supported
-		if ok, _ := c.Extension("STARTTLS"); ok {
-			if err = c.StartTLS(&tls.Config{InsecureSkipVerify: true, ServerName: *server}); err != nil {
-				c.Reset()
-				c.Quit()
-
-				log.Fatalf("Failed to establish TLS session: %s", err)
-			}
-
-			log.Println("TLS negotiated, sending over an encrypted channel")
-		} else {
-			log.Println("Server doesn't support TLS.. Sending over an unencrypted channel")
+		if err = client.AddAttachment(*attachment); err != nil {
+			log.Fatal(err)
 		}
 	}
 
-	// set the from addr
-	if err = c.Mail(*from); err != nil {
-		c.Reset()
-		c.Quit()
-
-		log.Fatalf("Failed to set the From address: %s", err)
+	if err = client.Send(); err != nil {
+		log.Fatal(err)
 	}
-
-	// add the recipients
-	for _, v := range strings.Split(*to, ",") {
-		if err = c.Rcpt(v); err != nil {
-			c.Reset()
-			c.Quit()
-
-			log.Fatalf("Failed to set a recipient: %s", err)
-		}
-	}
-
-	w, err := c.Data()
-
-	if err != nil {
-		c.Reset()
-		c.Quit()
-
-		log.Fatalf("Failed to issue DATA command: %s", err)
-	}
-
-	_, err = w.Write(buf.Bytes())
-
-	if err != nil {
-		c.Reset()
-		c.Quit()
-
-		log.Fatalf("Failed to write DATA: %s", err)
-	}
-
-	if err = w.Close(); err != nil {
-		c.Reset()
-		c.Quit()
-
-		log.Fatalf("Failed to close the DATA stream: %s", err)
-	}
-
-	c.Quit()
 
 	log.Println("Message Sent")
 }
